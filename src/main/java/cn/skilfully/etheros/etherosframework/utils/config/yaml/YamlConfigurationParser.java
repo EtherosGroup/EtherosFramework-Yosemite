cf5daa82ecb31325d8afb75a127cc1d3bdd409bb
IsCibocaz <IsCibocaz@outlook.com>
2026-06-29 22:23:25 +0800

    chore: 更新文件头

--- src/main/java/cn/skilfully/etheros/etherosframework/utils/config/yaml/YamlConfigurationParser.java
+++ src/main/java/cn/skilfully/etheros/etherosframework/utils/config/yaml/YamlConfigurationParser.java
@@ -16,16 +16,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package cn.skilfully.etheros.etherosframework.di.annotation;
+package cn.skilfully.etheros.etherosframework.utils.config.yaml;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.FIELD, ElementType.CONSTRUCTOR, ElementType.METHOD, ElementType.PARAMETER})
-public @interface GlobalAutowired {
-    String value() default "";
-    boolean required() default true;
+public class YamlConfigurationParser {
 }
